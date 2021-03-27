9e04e6167fedb06acca2cac32bd642d052e24721
Ace_DC <devin_chaudhary@yahoo.co.in>
2021-03-26 22:49:12 -0700

    Chapter 1 how mr. grinberg avoided circular import in __init__.py and other basics

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
 	- Kernel kya h?
 	- Conda jo 
-# Basics
-	Linux ka 
-
-	- 
+## Python basics
+	
+	- Library: is a collection of packages
+	- Package: is a collection of modules
+	- Module: is a .py file
+	- Object oriented paradigm. Class denotes to a real world entity. E.g. Cars are class. There are diparate forms of cars. Which type of car it is diesel, benzin.
+	Within a Class there are data and methods. Methods manipulate the data. E.g. Data is stored in a variable (a=10, a variable h, 10 data h). Method / function can change the variable. Now coming back to the Car example, accelerate or deccelerate both can be Methods / function. Only and only these methods should change the speed. And thus speed is the data and accelerate / de... are methods.
+	- Class: abstract idea e.g. Cars
+	- Object: specific entity e.g. 
--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,5 +1,31 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # Flask is class because convention is that Class's first letter is capital. flask is either a package or module and contains the class Flask.
+
+#Through this line one makes an object of Class Flask which is stored in object "app". for example if Flask is an abstract idea of apps then our microblog app is one specific app.
+
+#convention is that you call the class as if it was a function and pass the information required to create an object. In this example one supplied the information __name__ to class Flask to create the object app.
 
 from app import routes
+
+# app is either a module or a package. Because we are importing. If there is a __init__.py file in a folder, then the rule (although an exception in latest versions of python, maybe) is that it is a package.
+
+# hamne app naamak package se module routes.py ko amantrit karne ki koshish ki
+
+"""
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+"""
+
+# routes ne pahali line main hi app se app ko amantrit kiya parantu pahla app aur dusra app dono "not capital" hn arthat hum package se kuch import kar rahe hn arthat pahla app package h. jab yah package main __init.py__ main jaayega to dusri line main (code ki line no 3) app object mil jayega jo ki class Flask ka object h.
+
+#arthat routes.py main jab bhi app object import karne ke baad saare app object ko denote kar rahe hn
+
+# globals ka funda clear karna h
+# word wrap alt z
+# keyboard shortcuts ctrl ks searchable
+# how to add screenshot
