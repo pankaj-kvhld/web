f309799a761019a49710262a714050a244a0362d
Pankaj Joshi <pankaj.joshi.in@gmail.com>
2021-04-04 05:33:25 +0200

    routes updated

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,14 +1,19 @@
 from app import app
 from app.forms import LoginForm
-from flask import render_template
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template("base.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f"Login requested for user {form.username.data} remember_me={form.remember_me.data}"
+            )
+        return redirect("/index")
     return render_template("login.html", form=form)
