9f3e2ce17a4dda74fc32c4eb02879c7ddb0b4226
Pankaj Joshi <pankaj.joshi.in@gmail.com>
2021-04-04 22:45:26 +0200

    path of static corrected in index.html

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,15 +5,13 @@ from flask import render_template, flash, redirect
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("base.html")
+    return render_template("index.html")
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash(
-            f"Login requested for user {form.username.data} remember_me={form.remember_me.data}"
-            )
+        # flash(f"Login requested for user {form.username.data} remember_me={form.remember_me.data}")
         return redirect("/index")
     return render_template("login.html", form=form)
